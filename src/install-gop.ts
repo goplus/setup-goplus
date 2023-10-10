e11fd79b956ebe5990467ada63507f17310e2dd6
Li Jie <cpunion@gmail.com>
2023-10-10 17:01:55 +0800

    support semver

--- src/install-gop.ts
+++ src/install-gop.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core'
+import * as semver from 'semver'
 import fs from 'fs'
 import path from 'path'
 import os from 'os'
 import { execSync } from 'child_process'
+
+const GOPLUS_REPO = 'https://github.com/goplus/gop.git'
 
 /**
  * The main function for the action.
@@ -10,12 +13,20 @@ import { execSync } from 'child_process'
  */
 export async function installGop(): Promise<void> {
   try {
-    const versionSpec = resolveVersionInput()
-    const version = `v${versionSpec}`
-    const gopDir = clone(version)
+    const versionSpec = resolveVersionInput() || '1.1'
+    const versions = fetchVersions()
+    const version = semver.maxSatisfying(versions, versionSpec)
+    if (!version) {
+      throw new Error(
+        `Unable to find a version that satisfies the version spec '${versionSpec}'`
+      )
+    }
+    core.info(`Selected version ${version} by spec ${versionSpec}`)
+    const tagVersion = `v${version}`
+    const gopDir = clone(tagVersion)
     install(gopDir)
-    test(version)
-    core.setOutput('gop-version', versionSpec)
+    test(tagVersion)
+    core.setOutput('gop-version', version)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
@@ -30,7 +41,7 @@ function clone(versionSpec: string): string {
   }
   fs.mkdirSync(workDir)
   core.info(`Cloning gop ${versionSpec} to ${workDir} ...`)
-  const cmd = `git clone --depth 1 --branch ${versionSpec} https://github.com/goplus/gop.git`
+  const cmd = `git clone --depth 1 --branch ${versionSpec} ${GOPLUS_REPO}`
   execSync(cmd, { cwd: workDir, stdio: 'inherit' })
   core.info('gop cloned')
   return path.join(workDir, 'gop')
@@ -53,7 +64,6 @@ function install(gopDir: string): void {
 
 function test(versionSpec: string): void {
   core.info(`Testing gop ${versionSpec} ...`)
-  core.info(`PATH: ${process.env.PATH}`)
   const out = execSync('gop env GOPVERSION', { env: process.env })
   const actualVersion = out.toString().trim()
   if (actualVersion !== versionSpec) {
@@ -62,6 +72,17 @@ function test(versionSpec: string): void {
     )
   }
   core.info(`Installed gop version ${actualVersion}`)
+}
+
+function fetchVersions(): string[] {
+  const cmd = `git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' ${GOPLUS_REPO}`
+  const out = execSync(cmd).toString()
+  const versions = out
+    .split('\n')
+    .filter(s => s)
+    .map(s => s.split('\t')[1].replace('refs/tags/', ''))
+    .map(s => s.replace(/^v/, ''))
+  return versions
 }
 
 function resolveVersionInput(): string {
